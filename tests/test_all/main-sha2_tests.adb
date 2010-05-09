b9c9bd9d2869357100cd4fa3d724f80e9ec7afcb
Alexander Senier <mail@senier.net>
2010-05-09 23:46:31 +0200

    Added license to separate procedures.

--- tests/test_all/main-sha2_tests.adb
+++ tests/test_all/main-sha2_tests.adb
@@ -1,5 +1,22 @@
-separate (Main)
+--  This file is part of the sparkcrypto library.
 
+--  Copyright (C) 2010  secunet Security Networks AG
+--  Copyright (C) 2010  Alexander Senier <mail@senier.net>
+
+--  This library  is free software:  you can  redistribute it and/or  modify it
+--  under the  terms of the GNU  Lesser General Public License  as published by
+--  the Free Software Foundation, either version  3 of the License, or (at your
+--  option) any later version.
+
+--  This library is distributed in the hope that it will be useful, but WITHOUT
+--  ANY  WARRANTY; without  even  the implied  warranty  of MERCHANTABILITY  or
+--  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+--  for more details.
+
+--  You should  have received a copy  of the GNU Lesser  General Public License
+--  along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+separate (Main)
 procedure SHA2_Tests is
    SHA2_Ctx1, SHA2_Ctx2, SHA2_Ctx3  : SHA2.Context_Type;
    Hash1, Hash2, Hash3              : SHA2.Hash_Type;
