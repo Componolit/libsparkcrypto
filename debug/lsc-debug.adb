aaa831c260ff7bda15a26ff8e6de75570045038d
Alexander Senier <mail@senier.net>
2010-05-09 23:45:59 +0200

    Improved readability of debug output and re-added debug verion of debug.adb.

--- debug/lsc-debug.adb
+++ debug/lsc-debug.adb
@@ -1,5 +1,5 @@
 --  This file is part of the sparkcrypto library.
-
+--
 --  Copyright (C) 2010  secunet Security Networks AG
 --  Copyright (C) 2010  Alexander Senier <mail@senier.net>
 
@@ -16,26 +16,15 @@
 --  You should  have received a copy  of the GNU Lesser  General Public License
 --  along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-with Ada.Text_IO, LSC.Debug, IO;
+with Types, IO;
+use type Types.Word64;
 
-package body AES256.Debug is
+package body LSC.Debug is
 
-   procedure Print_Schedule (S : AES256.Schedule_Type) is
+   procedure Put (T : String) renames IO.Put;
+   procedure Put_Line (T : String) renames IO.Put_Line;
+   procedure New_Line renames IO.New_Line;
+   procedure Print_Word32 (I : in Types.Word32) renames IO.Print_Word32;
+   procedure Print_Word64 (I : in Types.Word64) renames IO.Print_Word64;
 
-      package SIIO is new Ada.Text_IO.Integer_IO (AES256.Schedule_Index);
-   begin
-      for Index in AES256.Schedule_Index
-      loop
-         IO.Put ("   W");
-         SIIO.Put (Index, Width => 3);
-         IO.Put (" = ");
-         LSC.Debug.Print_Word32 (S (Index));
-         if Index mod  AES256.Nb = 0
-         then
-            IO.New_Line;
-         end if;
-      end loop;
-      IO.New_Line;
-   end Print_Schedule;
-
-end AES256.Debug;
+end LSC.Debug;
