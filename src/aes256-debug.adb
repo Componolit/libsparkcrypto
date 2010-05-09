5b44b9164d34d759853f72031b9bcb9e3ddf1b5e
Alexander Senier <mail@senier.net>
2010-05-09 22:51:17 +0200

    Added license information.

--- src/aes256-debug.adb
+++ src/aes256-debug.adb
@@ -1,3 +1,21 @@
+--  This file is part of the sparkcrypto library.
+
+--  Copyright (C) 2010  secunet Security Networks AG
+--  Copyright (C) 2010  Alexander Senier <mail@senier.net>
+
+--  This library  is free software:  you can  redistribute it and/or  modify it
+--  under the  terms of the GNU  Lesser General Public License  as published by
+--  the Free Software Foundation, either version  3 of the License, or (at your
+--  option) any later version.
+
+--  This library is distributed in the hope that it will be useful, but WITHOUT
+--  ANY  WARRANTY; without  even  the implied  warranty  of MERCHANTABILITY  or
+--  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+--  for more details.
+
+--  You should  have received a copy  of the GNU Lesser  General Public License
+--  along with this library. If not, see <http://www.gnu.org/licenses/>.
+
 package body AES256.Debug is
 
    procedure Print_Schedule (S : AES256.Schedule_Type) is
