520a4e9c2cf7a19a5621b708133998e6c6096d85
Alexander Senier <alexander.senier@secunet.com>
2010-05-20 17:44:16 +0200

    Adopted tests for encryption (currently all tests fail).

--- progs/test_aes/main-aes_tests.adb
+++ progs/test_aes/main-aes_tests.adb
@@ -19,117 +19,101 @@
 separate (Main)
 procedure AES_Tests
 is
-   Plaintext, Cyphertext, Expected_Cyphertext : LSC.AES.Block_Type;
-   Key128                                     : LSC.AES.AES128_Key_Type;
-   Key192                                     : LSC.AES.AES192_Key_Type;
-   Key256                                     : LSC.AES.AES256_Key_Type;
-   Context                                    : LSC.AES.AES_Enc_Context;
+   Plaintext, Ciphertext                     : LSC.AES.Block_Type;
+   Key128                                    : LSC.AES.AES128_Key_Type;
+   Key192                                    : LSC.AES.AES192_Key_Type;
+   Key256                                    : LSC.AES.AES256_Key_Type;
+   Enc_Context                               : LSC.AES.AES_Enc_Context;
+   Dec_Context                               : LSC.AES.AES_Dec_Context;
 
 begin
 
    LSC.Test.Suite ("AES tests");
 
+   --------------
    -- AES128 test
+   --------------
+
+   --  Key expansion
+
+   --# accept Flow, 10, Key192, "Only debug output needed";
    Key128 := LSC.AES.AES128_Key_Type'
-      (16#00010203#,
-       16#04050607#,
-       16#08090a0b#,
-       16#0c0d0e0f#);
+      (16#2b7e1516#, 16#28aed2a6#, 16#abf71588#, 16#09cf4f3c#);
+
+   --# accept Flow, 10, "Only debug output needed";
+   Enc_Context := LSC.AES.Create_AES128_Enc_Context (Key128);
+
+   Key128 := LSC.AES.AES128_Key_Type'
+      (16#00010203#, 16#04050607#, 16#08090a0b#, 16#0c0d0e0f#);
 
    Plaintext := LSC.AES.Block_Type'
-      (16#00112233#,
-       16#44556677#,
-       16#8899aabb#,
-       16#ccddeeff#);
+      (16#00112233#, 16#44556677#, 16#8899aabb#, 16#ccddeeff#);
 
-   Expected_Cyphertext := LSC.AES.Block_Type'
-      (16#69c4e0d8#,
-       16#6a7b0430#,
-       16#d8cdb780#,
-       16#70b4c55a#);
+   Ciphertext := LSC.AES.Block_Type'
+      (16#69c4e0d8#, 16#6a7b0430#, 16#d8cdb780#, 16#70b4c55a#);
 
-   Context := LSC.AES.Create_AES128_Enc_Context (Key => Key128);
-   Cyphertext := LSC.AES.Encrypt (Context => Context, Plaintext => Plaintext);
+   --  Encryption
+   Enc_Context := LSC.AES.Create_AES128_Enc_Context (Key128);
+   LSC.Test.Run ("C.1 AES-128 (Nk=4, Nr=10), Encryption",
+                 Ciphertext = LSC.AES.Encrypt (Enc_Context, Plaintext));
 
-   LSC.Test.Run ("C.1 AES-128 (Nk=4, Nr=10)", Cyphertext = Expected_Cyphertext);
+   -- Decryption
+   Dec_Context := LSC.AES.Create_AES128_Dec_Context (Key128);
+   LSC.Test.Run ("C.1 AES-128 (Nk=4, Nr=10), Decryption",
+                 Plaintext = LSC.AES.Decrypt (Dec_Context, Ciphertext));
+
+   --------------
+   -- AES192 test
+   --------------
 
    --# accept Flow, 10, Key192, "Only debug output needed";
    Key192 := LSC.AES.AES192_Key_Type'
-      (16#8e73b0f7#,
-       16#da0e6452#,
-       16#c810f32b#,
-       16#809079e5#,
-       16#62f8ead2#,
-       16#522c6b7b#);
+      (16#8e73b0f7#, 16#da0e6452#, 16#c810f32b#,
+       16#809079e5#, 16#62f8ead2#, 16#522c6b7b#);
 
    --# accept Flow, 10, "Only debug output needed";
-   Context := LSC.AES.Create_AES192_Enc_Context (Key => Key192);
+   Enc_Context := LSC.AES.Create_AES192_Enc_Context (Key192);
 
-   -- AES192 test
    Key192 := LSC.AES.AES192_Key_Type'
-      (16#00010203#,
-       16#04050607#,
-       16#08090a0b#,
-       16#0c0d0e0f#,
-       16#10111213#,
-       16#14151617#);
+      (16#00010203#, 16#04050607#, 16#08090a0b#,
+       16#0c0d0e0f#, 16#10111213#, 16#14151617#);
 
    Plaintext := LSC.AES.Block_Type'
-      (16#00112233#,
-       16#44556677#,
-       16#8899aabb#,
-       16#ccddeeff#);
+      (16#00112233#, 16#44556677#, 16#8899aabb#, 16#ccddeeff#);
 
-   Expected_Cyphertext := LSC.AES.Block_Type'
-      (16#dda97ca4#,
-       16#864cdfe0#,
-       16#6eaf70a0#,
-       16#ec0d7191#);
+   Ciphertext := LSC.AES.Block_Type'
+      (16#dda97ca4#, 16#864cdfe0#, 16#6eaf70a0#, 16#ec0d7191#);
 
-   Context := LSC.AES.Create_AES192_Enc_Context (Key => Key192);
-   Cyphertext := LSC.AES.Encrypt (Context => Context, Plaintext => Plaintext);
+   --  Encryption
+   Enc_Context := LSC.AES.Create_AES192_Enc_Context (Key192);
+   LSC.Test.Run ("C.2 AES-192 (Nk=6, Nr=12), Encryption",
+                 Ciphertext = LSC.AES.Encrypt (Enc_Context, Plaintext));
 
-   LSC.Test.Run ("C.2 AES-192 (Nk=6, Nr=12)", Cyphertext = Expected_Cyphertext);
+   --------------
+   -- AES256 test
+   --------------
 
    --# accept Flow, 10, Key256, "Only debug output needed";
    Key256 := LSC.AES.AES256_Key_Type'
-      (16#603deb10#,
-       16#15ca71be#,
-       16#2b73aef0#,
-       16#857d7781#,
-       16#1f352c07#,
-       16#3b6108d7#,
-       16#2d9810a3#,
-       16#0914dff4#);
+      (16#603deb10#, 16#15ca71be#, 16#2b73aef0#, 16#857d7781#,
+       16#1f352c07#, 16#3b6108d7#, 16#2d9810a3#, 16#0914dff4#);
 
    --# accept Flow, 10, "Only debug output needed";
-   Context := LSC.AES.Create_AES256_Enc_Context (Key => Key256);
+   Enc_Context := LSC.AES.Create_AES256_Enc_Context (Key256);
 
    Key256 := LSC.AES.AES256_Key_Type'
-      (16#00010203#,
-       16#04050607#,
-       16#08090a0b#,
-       16#0c0d0e0f#,
-       16#10111213#,
-       16#14151617#,
-       16#18191a1b#,
-       16#1c1d1e1f#);
+      (16#00010203#, 16#04050607#, 16#08090a0b#, 16#0c0d0e0f#,
+       16#10111213#, 16#14151617#, 16#18191a1b#, 16#1c1d1e1f#);
 
    Plaintext := LSC.AES.Block_Type'
-      (16#00112233#,
-       16#44556677#,
-       16#8899aabb#,
-       16#ccddeeff#);
+      (16#00112233#, 16#44556677#, 16#8899aabb#, 16#ccddeeff#);
 
-   Expected_Cyphertext := LSC.AES.Block_Type'
-      (16#8ea2b7ca#,
-       16#516745bf#,
-       16#eafc4990#,
-       16#4b496089#);
+   Ciphertext := LSC.AES.Block_Type'
+      (16#8ea2b7ca#, 16#516745bf#, 16#eafc4990#, 16#4b496089#);
 
-   Context := LSC.AES.Create_AES256_Enc_Context (Key => Key256);
-   Cyphertext := LSC.AES.Encrypt (Context => Context, Plaintext => Plaintext);
-
-   LSC.Test.Run ("C.3 AES-256 (Nk=8, Nr=14)", Cyphertext = Expected_Cyphertext);
+   --  Encryption
+   Enc_Context := LSC.AES.Create_AES256_Enc_Context (Key256);
+   LSC.Test.Run ("C.3 AES-256 (Nk=8, Nr=14), Encryption",
+                 Ciphertext = LSC.AES.Encrypt (Enc_Context, Plaintext));
 
 end AES_Tests;
