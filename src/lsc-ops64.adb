7de5ec8366db380f1093907898bbb07c2499275b
Alexander Senier <alexander.senier@secunet.com>
2010-06-01 16:49:58 +0200

    Added Ops64 package.

---
+++ src/lsc-ops64.adb
@@ -0,0 +1,28 @@
+--  This file is part of the sparkcrypto library.
+
+--  Copyright (C) 2010  secunet Security Networks AG
+--  Copyright (C) 2010  Alexander Senier <mail@senier.net>
+
+--  This library  is free software:  you can  redistribute it and/or  modify it
+--  under the  terms of the GNU  Lesser General Public License  as published by
+--  the Free Software Foundation, either version  3 of the License, or (at your
+--  option) any later version.
+
+--  This library is distributed in the hope that it will be useful, but WITHOUT
+--  ANY  WARRANTY; without  even  the implied  warranty  of MERCHANTABILITY  or
+--  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+--  for more details.
+
+--  You should  have received a copy  of the GNU Lesser  General Public License
+--  along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+package body LSC.Ops64 is
+
+   function XOR2 (V0, V1 : Types.Word64) return Types.Word64
+   is
+   begin
+      return V0 xor V1;
+   end XOR2;
+   pragma Inline (XOR2);
+
+end LSC.Ops64;
