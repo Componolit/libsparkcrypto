ad18f9e827a8b9112a9a3796b31f0d3cfa8feeeb
Alexander Senier <mail@senier.net>
2010-06-05 00:01:49 +0200

    First, untested version of HMAC-RIPEMD160.

---
+++ src/lsc-hmac-ripemd.adb
@@ -0,0 +1,112 @@
+--  This file is part of the sparkcrypto library.
+
+--  Copyright (C) 2010  secunet Security Networks AG
+--  Copyright (C) 2010  Alexander Senier <mail@senier.net>
+
+--  This library  is free software:  you can  redistribute it and/or  modify it
+--  under the  terms of the GNU  Lesser General Public License  as published by
+--  the Free Software Foundation, either version  3 of the License, or (at your
+--  option) any later version.
+
+--  This library is distributed in the hope that it will be useful, but WITHOUT
+--  ANY  WARRANTY; without  even  the implied  warranty  of MERCHANTABILITY  or
+--  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+--  for more details.
+
+--  You should  have received a copy  of the GNU Lesser  General Public License
+--  along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+package body LSC.HMAC.RIPEMD is
+
+   IPad : constant RIPEMD160.Block_Type :=
+      RIPEMD160.Block_Type'(others => 16#36363636#);
+   OPad : constant RIPEMD160.Block_Type :=
+      RIPEMD160.Block_Type'(others => 16#5C5C5C5C#);
+
+   ----------------------------------------------------------------------------
+
+   function To_Block (Item : RIPEMD160.Hash_Type) return RIPEMD160.Block_Type is
+      Result : RIPEMD160.Block_Type := RIPEMD160.Block_Type'(others => 0);
+   begin
+      for I in RIPEMD160.Hash_Index
+      loop
+         Result (I) := Item (I);
+         --# assert
+         --#    (I in RIPEMD160.Hash_Index) and
+         --#    (I in RIPEMD160.Block_Index) and
+         --#    (for all Pos in RIPEMD160.Hash_Index range RIPEMD160.Hash_Index'First .. I =>
+         --#         (Result (Pos) = Item (Pos)));
+      end loop;
+      return Result;
+   end To_Block;
+
+   ----------------------------------------------------------------------------
+
+   function Context_Init (Key : RIPEMD160.Block_Type) return Context_Type is
+      Result : Context_Type;
+      Temp   : RIPEMD160.Block_Type := RIPEMD160.Block_Type'(others => 0);
+   begin
+      Debug.Put_Line ("HMAC.RIPEMD160.Context_Init:");
+
+      Result.Key            := Key;
+      Result.RIPEMD160_Context := RIPEMD160.Context_Init;
+      Ops.Block_XOR (IPad, Result.Key, Temp);
+      RIPEMD160.Context_Update (Result.RIPEMD160_Context, Temp);
+      return Result;
+   end Context_Init;
+
+   ----------------------------------------------------------------------------
+
+   procedure Context_Update
+     (Context : in out Context_Type;
+      Block   : in RIPEMD160.Block_Type)
+   is
+   begin
+      Debug.Put_Line ("HMAC.RIPEMD160.Context_Update:");
+      RIPEMD160.Context_Update (Context.RIPEMD160_Context, Block);
+   end Context_Update;
+
+   ----------------------------------------------------------------------------
+
+   procedure Context_Finalize
+     (Context : in out Context_Type;
+      Block   : in     RIPEMD160.Block_Type;
+      Length  : in     RIPEMD160.Block_Length_Type)
+   is
+      Hash : RIPEMD160.Hash_Type;
+      Temp : RIPEMD160.Block_Type := RIPEMD160.Block_Type'(others => 0);
+   begin
+      Debug.Put_Line ("HMAC.RIPEMD160.Context_Finalize:");
+      RIPEMD160.Context_Finalize (Context.RIPEMD160_Context, Block, Length);
+      Hash := RIPEMD160.Get_Hash (Context.RIPEMD160_Context);
+
+      Context.RIPEMD160_Context := RIPEMD160.Context_Init;
+      Ops.Block_XOR (OPad, Context.Key, Temp);
+      RIPEMD160.Context_Update (Context.RIPEMD160_Context, Temp);
+      RIPEMD160.Context_Finalize (Context.RIPEMD160_Context, To_Block (Hash), 160);
+   end Context_Finalize;
+
+   ----------------------------------------------------------------------------
+
+   function Get_Prf (Context : in Context_Type) return RIPEMD160.Hash_Type is
+   begin
+      return RIPEMD160.Get_Hash (Context.RIPEMD160_Context);
+   end Get_Prf;
+
+   ----------------------------------------------------------------------------
+
+   function Get_Auth (Context : in Context_Type) return Auth_Type is
+      Result : Auth_Type := Auth_Type'(others => 0);
+      Prf    : RIPEMD160.Hash_Type;
+   begin
+      Prf := RIPEMD160.Get_Hash (Context.RIPEMD160_Context);
+      for Index in Auth_Index
+      --# assert
+      --#    Index in Auth_Index;
+      loop
+         Result (Index) := Prf (Index);
+      end loop;
+      return Result;
+   end Get_Auth;
+
+end LSC.HMAC.RIPEMD;
---
+++ src/lsc-hmac-ripemd.ads
@@ -0,0 +1,64 @@
+--  This file is part of the sparkcrypto library.
+
+--  Copyright (C) 2010  secunet Security Networks AG
+--  Copyright (C) 2010  Alexander Senier <mail@senier.net>
+
+--  This library  is free software:  you can  redistribute it and/or  modify it
+--  under the  terms of the GNU  Lesser General Public License  as published by
+--  the Free Software Foundation, either version  3 of the License, or (at your
+--  option) any later version.
+
+--  This library is distributed in the hope that it will be useful, but WITHOUT
+--  ANY  WARRANTY; without  even  the implied  warranty  of MERCHANTABILITY  or
+--  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+--  for more details.
+
+--  You should  have received a copy  of the GNU Lesser  General Public License
+--  along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+with LSC.RIPEMD160, LSC.Types, LSC.Ops, LSC.Debug;
+use type LSC.Types.Word32;
+
+--# inherit LSC.Debug,
+--#         LSC.RIPEMD160,
+--#         LSC.Ops,
+--#         LSC.Types;
+
+package LSC.HMAC.RIPEMD is
+
+   type Context_Type is private;
+
+   subtype Auth_Index is Types.Index range 0 .. 2;
+   subtype Auth_Type is Types.Word32_Array_Type (Auth_Index);
+
+   function Context_Init (Key : RIPEMD160.Block_Type) return Context_Type;
+
+   procedure Context_Update
+     (Context : in out Context_Type;
+      Block   : in     RIPEMD160.Block_Type);
+   --# derives Context from *,
+   --#                      Block;
+
+   procedure Context_Finalize
+     (Context : in out Context_Type;
+      Block   : in     RIPEMD160.Block_Type;
+      Length  : in     RIPEMD160.Block_Length_Type);
+   --# derives Context from *,
+   --#                      Block,
+   --#                      Length;
+
+   function Get_Prf  (Context : in Context_Type) return RIPEMD160.Hash_Type;
+   function Get_Auth (Context : in Context_Type) return Auth_Type;
+
+private
+
+   type Context_Type is record
+      RIPEMD160_Context : RIPEMD160.Context_Type;
+      Key               : RIPEMD160.Block_Type;
+   end record;
+
+   function To_Block (Item : RIPEMD160.Hash_Type) return RIPEMD160.Block_Type;
+   --# return Result =>
+   --#     (for all I in RIPEMD160.Hash_Index => (Result (I) = Item (I)));
+
+end LSC.HMAC.RIPEMD;
