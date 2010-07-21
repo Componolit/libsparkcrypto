4f708eea6bee05006b5c453268cae628962d1be6
Alexander Senier <alexander.senier@secunet.com>
2010-07-21 11:21:51 +0200

    Changed license to GMGPL (based on GPLv3).

--- src/debug/lsc-aes-print.adb
+++ src/debug/lsc-aes-print.adb
@@ -1,20 +1,29 @@
+-------------------------------------------------------------------------------
 --  This file is part of the sparkcrypto library.
-
---  Copyright (C) 2010  secunet Security Networks AG
+--
 --  Copyright (C) 2010  Alexander Senier <mail@senier.net>
-
---  This library  is free software:  you can  redistribute it and/or  modify it
---  under the  terms of the GNU  Lesser General Public License  as published by
---  the Free Software Foundation, either version  3 of the License, or (at your
---  option) any later version.
-
---  This library is distributed in the hope that it will be useful, but WITHOUT
---  ANY  WARRANTY; without  even  the implied  warranty  of MERCHANTABILITY  or
---  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
---  for more details.
-
---  You should  have received a copy  of the GNU Lesser  General Public License
---  along with this library. If not, see <http://www.gnu.org/licenses/>.
+--  Copyright (C) 2010  secunet Security Networks AG
+--
+--  This program is free software: you can redistribute it and/or modify it
+--  under the terms of the GNU General Public License as published by the Free
+--  Software Foundation, either version 3 of the License, or (at your option)
+--  any later version.
+--
+--  This program is distributed in the hope that it will be useful, but WITHOUT
+--  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+--  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+--  more details.
+--  
+--  You should have received a copy of the GNU General Public License along
+--  with this program.  If not, see <http://www.gnu.org/licenses/>.
+--  
+--  As a special exception, if other files instantiate generics from this unit,
+--  or you link this unit with other files to produce an executable, this unit
+--  does not by itself cause the resulting executable to be covered by the GNU
+--  General Public License. This exception does not however invalidate any
+--  other reasons why the executable file might be covered by the GNU Public
+--  License.
+-------------------------------------------------------------------------------
 
 with Ada.Text_IO, LSC.Debug, LSC.IO;
 
