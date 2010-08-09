546993b7dfbcadeeead5b680314d4185e220ce0e
Alexander Senier <alexander.senier@secunet.com>
2010-08-09 18:00:13 +0200

    Unified Message_Index for all HMACs

--- src/shared/generic/lsc-sha256.ads
+++ src/shared/generic/lsc-sha256.ads
@@ -59,7 +59,7 @@ package LSC.SHA256 is
 
    --  A SHA256 message can be at most 2^64 bit long. As one block has 511 bit,
    --  this makes 2^53 blocks.
-   subtype Message_Index is Types.Word64 range 1 .. 2 ** 53;
+   subtype Message_Index is Types.Word64 range 0 .. 2 ** 53 - 1;
    type Message_Type is array (Message_Index range <>) of Block_Type;
 
    -- Initialize SHA256 context.
