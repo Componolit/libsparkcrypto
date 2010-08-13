887990b06bd9bfcd2566c43a396509dffb2027f5
Alexander Senier <mail@senier.net>
2010-08-13 22:46:01 +0200

    Documented LSC.AES

--- src/shared/generic/lsc-aes.ads
+++ src/shared/generic/lsc-aes.ads
@@ -31,53 +31,90 @@ use type LSC.Types.Index;
 --#    LSC.Byteorder32;
 
 -------------------------------------------------------------------------------
---  References:
+--  The AES algorithm
 --
---  FIPS PUB 197, Advanced Encryption Standard (AES), National Institute of
+--  <ul>
+--  <li> FIPS PUB 197, Advanced Encryption Standard (AES),<br> National Institute of
 --  Standards and Technology, U.S. Department of Commerce, November 2001.
---  [doc/specs/fips-197.pdf]
+--  [doc/specs/fips-197.pdf] </li>
 --
---  Joan Daemen and Vincent Rijmen, AES submission document on Rijndael,
---  Version 2, September 1999.
---  [doc/specs/Rijndael-amended.pdf]
+--  <li> Joan Daemen and Vincent Rijmen, AES submission document on Rijndael,<br>
+--  Version 2, September 1999. [doc/specs/Rijndael-amended.pdf] </li>
+--  </ul>
 -------------------------------------------------------------------------------
 package LSC.AES is
 
+   --  AES encryption context
    type AES_Enc_Context is private;
+
+   --  AES decryption context
    type AES_Dec_Context is private;
 
+   --  Index of AES key
    subtype Key_Index is Types.Index range 0 .. 7;
+
+   --  AES key
    type Key_Type is array (Key_Index range <>) of Types.Word32;
 
+   --  Index of AES-128 key
    subtype AES128_Key_Index is Types.Index range 0 .. 3;
+
+   --  AES-128 key
    subtype AES128_Key_Type is Key_Type (AES128_Key_Index);
 
+   --  Index of AES-192 key
    subtype AES192_Key_Index is Types.Index range 0 .. 5;
+
+   --  AES-192 key
    subtype AES192_Key_Type is Key_Type (AES192_Key_Index);
 
+   --  Index of AES-256 key
    subtype AES256_Key_Index is Types.Index range 0 .. 7;
+
+   --  AES-256 key
    subtype AES256_Key_Type is Key_Type (AES256_Key_Index);
 
+   --  Index of AES block
    subtype Block_Index is Types.Index range 0 .. 3;
+
+   --  AES block
    subtype Block_Type is Types.Word32_Array_Type (Block_Index);
 
+   --  Index of AES message
    subtype Message_Index is Natural;
+
+   --  AES message (unconstrained array of AES blocks)
    type Message_Type is array (Message_Index range <>) of Block_Type;
 
+   --  Create AES-128 encryption context from AES-128 @Key@
    function Create_AES128_Enc_Context (Key : AES128_Key_Type) return AES_Enc_Context;
+
+   --  Create AES-192 encryption context from AES-192 @Key@
    function Create_AES192_Enc_Context (Key : AES192_Key_Type) return AES_Enc_Context;
+
+   --  Create AES-256 encryption context from AES-256 @Key@
    function Create_AES256_Enc_Context (Key : AES256_Key_Type) return AES_Enc_Context;
 
+   --  Encrypt one @Plaintext@ block using given @Context@, return one block of
+   --  ciphertext
    function Encrypt (Context   : AES_Enc_Context;
                      Plaintext : Block_Type) return Block_Type;
 
+   --  Create AES-128 decryption context from AES-128 @Key@
    function Create_AES128_Dec_Context (Key : AES128_Key_Type) return AES_Dec_Context;
+
+   --  Create AES-192 decryption context from AES-192 @Key@
    function Create_AES192_Dec_Context (Key : AES192_Key_Type) return AES_Dec_Context;
+
+   --  Create AES-256 decryption context from AES-256 @Key@
    function Create_AES256_Dec_Context (Key : AES256_Key_Type) return AES_Dec_Context;
 
+   --  Decrypt one @Ciphertext@ block using given @Context@, return one block of
+   --  plaintext
    function Decrypt (Context    : AES_Dec_Context;
                      Ciphertext : Block_Type) return Block_Type;
 
+   --  Empty AES block
    Null_Block : constant Block_Type;
 
 private
