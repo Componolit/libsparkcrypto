236e397243f538a0efac0ced55c4f8bf8576b591
Stefan Berghofer <stefan.berghofer@secunet.com>
2011-04-01 15:11:54 +0200

    Removed unused function Shift_Right; tuned.

--- shadow/interfaces.ads
+++ shadow/interfaces.ads
@@ -1,9 +1,5 @@
 package Interfaces
 is
-   type Unsigned_32 is mod 2**32;
-   type Unsigned_64 is mod 2**64;
-
-   function Shift_Right (Value : Unsigned_64; Amount : Natural) return Unsigned_64;
-   --# return Result => Result = Value / 2 ** Amount;
-
+   type Unsigned_32 is mod 2 ** 32;
+   type Unsigned_64 is mod 2 ** 64;
 end Interfaces;
