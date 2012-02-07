fd8479e614ea873ac8896486d711515488d996fc
Stefan Berghofer <stefan.berghofer@secunet.com>
2012-02-07 13:13:32 +0100

    Added simultaenous point multiplication and inversion

--- src/shared/generic/lsc-ec.adb
+++ src/shared/generic/lsc-ec.adb
@@ -301,23 +301,127 @@ is
 
    ----------------------------------------------------------------------------
 
-   procedure Make_Affine
-     (X1      : in     Coord;
-      Y1      : in     Coord;
-      Z1      : in     Coord;
-      X2      :    out Coord;
-      Y2      :    out Coord;
-      R       : in     Coord;
-      M       : in     Coord;
-      M_Inv   : in     Types.Word32)
+   procedure Two_Point_Mult
+     (X1       : in     Coord;
+      Y1       : in     Coord;
+      Z1       : in     Coord;
+      E1       : in     Bignum.Big_Int;
+      E1_First : in     Natural;
+      E1_Last  : in     Natural;
+      X2       : in     Coord;
+      Y2       : in     Coord;
+      Z2       : in     Coord;
+      E2       : in     Bignum.Big_Int;
+      E2_First : in     Natural;
+      X3       :    out Coord;
+      Y3       :    out Coord;
+      Z3       :    out Coord;
+      A        : in     Coord;
+      M        : in     Coord;
+      M_Inv    : in     Types.Word32)
    is
-      Carry : Boolean;
-      H1, H2, H3, H4, H5 : Coord;
+      X4, Y4, Z4, X5, Y5, Z5 : Coord;
    begin
-      Bignum.Initialize (H4, H4'First, H4'Last);
-      H4 (H4'First) := 2;
+      Point_Add
+        (X1 => X1, Y1 => Y1, Z1 => Z1,
+         X2 => X2, Y2 => Y2, Z2 => Z2,
+         X3 => X5, Y3 => Y5, Z3 => Z5,
+         A => A, M => M, M_Inv => M_Inv);
+
+      Bignum.Initialize (X3, X3'First, X3'Last);
+      Bignum.Initialize (Y3, Y3'First, Y3'Last);
+      Bignum.Initialize (Z3, Z3'First, Z3'Last);
+
+      for I in reverse Natural range E1_First .. E1_Last
+      --# assert
+      --#   Bignum.Num_Of_Big_Int (X3, X3'First, X3'Length) <
+      --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+      --#   Bignum.Num_Of_Big_Int (Y3, Y3'First, Y3'Length) <
+      --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+      --#   Bignum.Num_Of_Big_Int (Z3, Z3'First, Z3'Length) <
+      --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+      --#   Bignum.Num_Of_Big_Int (X5, X5'First, X5'Length) <
+      --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+      --#   Bignum.Num_Of_Big_Int (Y5, Y5'First, Y5'Length) <
+      --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+      --#   Bignum.Num_Of_Big_Int (Z5, Z5'First, Z5'Length) <
+      --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length);
+      loop
+         for J in reverse Natural range 0 .. 31
+         --# assert
+         --#   Bignum.Num_Of_Big_Int (X3, X3'First, X3'Length) <
+         --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+         --#   Bignum.Num_Of_Big_Int (Y3, Y3'First, Y3'Length) <
+         --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+         --#   Bignum.Num_Of_Big_Int (Z3, Z3'First, Z3'Length) <
+         --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+         --#   Bignum.Num_Of_Big_Int (X5, X5'First, X5'Length) <
+         --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+         --#   Bignum.Num_Of_Big_Int (Y5, Y5'First, Y5'Length) <
+         --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+         --#   Bignum.Num_Of_Big_Int (Z5, Z5'First, Z5'Length) <
+         --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length);
+         loop
+            Point_Double (X3, Y3, Z3, X4, Y4, Z4, A, M, M_Inv);
+
+            if (E1 (I) and 2 ** J) /= 0 then
+               if (E2 (E2_First + (I - E1_First)) and 2 ** J) /= 0 then
+                  Point_Add
+                    (X1 => X4, Y1 => Y4, Z1 => Z4,
+                     X2 => X5, Y2 => Y5, Z2 => Z5,
+                     X3 => X3, Y3 => Y3, Z3 => Z3,
+                     A => A, M => M, M_Inv => M_Inv);
+               else
+                  Point_Add
+                    (X1 => X4, Y1 => Y4, Z1 => Z4,
+                     X2 => X1, Y2 => Y1, Z2 => Z1,
+                     X3 => X3, Y3 => Y3, Z3 => Z3,
+                     A => A, M => M, M_Inv => M_Inv);
+               end if;
+            elsif (E2 (E2_First + (I - E1_First)) and 2 ** J) /= 0 then
+               Point_Add
+                 (X1 => X4, Y1 => Y4, Z1 => Z4,
+                  X2 => X2, Y2 => Y2, Z2 => Z2,
+                  X3 => X3, Y3 => Y3, Z3 => Z3,
+                  A => A, M => M, M_Inv => M_Inv);
+            else
+               Bignum.Copy (X4, X4'First, X4'Last, X3, X3'First);
+               Bignum.Copy (Y4, Y4'First, Y4'Last, Y3, Y3'First);
+               Bignum.Copy (Z4, Z4'First, Z4'Last, Z3, Z3'First);
+            end if;
+
+            --# assert
+            --#   Bignum.Num_Of_Big_Int (X3, X3'First, X3'Length) <
+            --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+            --#   Bignum.Num_Of_Big_Int (Y3, Y3'First, Y3'Length) <
+            --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+            --#   Bignum.Num_Of_Big_Int (Z3, Z3'First, Z3'Length) <
+            --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+            --#   Bignum.Num_Of_Big_Int (X5, X5'First, X5'Length) <
+            --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+            --#   Bignum.Num_Of_Big_Int (Y5, Y5'First, Y5'Length) <
+            --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+            --#   Bignum.Num_Of_Big_Int (Z5, Z5'First, Z5'Length) <
+            --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length);
+         end loop;
+      end loop;
+   end Two_Point_Mult;
+
+   ----------------------------------------------------------------------------
+
+   procedure Invert
+     (A     : in     Coord;
+      B     :    out Coord;
+      R     : in     Coord;
+      M     : in     Coord;
+      M_Inv : in     Types.Word32)
+   is
+      Two : constant Coord := Coord'(2, others => 0);
+      E, H1, H2, H3, H4 : Coord;
+      Carry : Boolean;
+   begin
       --# accept Flow, 10, Carry, "Carry not needed here";
-      Bignum.Sub (H5, H5'First, H5'Last, M, M'First, H4, H4'First, Carry);
+      Bignum.Sub (E, E'First, E'Last, M, M'First, Two, Two'First, Carry);
       --# end accept;
 
       --# accept Flow, 10, H1, "auxiliary variable" &
@@ -327,11 +431,11 @@ is
         (A          => H4,
          A_First    => H4'First,
          A_Last     => H4'Last,
-         X          => Z1,
-         X_First    => Z1'First,
-         E          => H5,
-         E_First    => H5'First,
-         E_Last     => H5'Last,
+         X          => A,
+         X_First    => A'First,
+         E          => E,
+         E_First    => E'First,
+         E_Last     => E'Last,
          M          => M,
          M_First    => M'First,
          Aux1       => H1,
@@ -346,20 +450,38 @@ is
       --# end accept;
 
       Bignum.Mont_Mult
-        (H1, H1'First, H1'Last, H4, H4'First, R, R'First,
-         M, M'First, M_Inv);
-
-      Bignum.Mont_Mult
-        (X2, X2'First, X2'Last, X1, X1'First, H1, H1'First,
-         M, M'First, M_Inv);
-
-      Bignum.Mont_Mult
-        (Y2, Y2'First, Y2'Last, Y1, Y1'First, H1, H1'First,
+        (B, B'First, B'Last, H4, H4'First, R, R'First,
          M, M'First, M_Inv);
 
       --# accept Flow, 33, Carry, "Carry not needed here" &
+      --#        Flow, 33, H1, "auxiliary variable" &
       --#        Flow, 33, H2, "auxiliary variable" &
       --#        Flow, 33, H3, "auxiliary variable";
+   end Invert;
+
+   ----------------------------------------------------------------------------
+
+   procedure Make_Affine
+     (X1      : in     Coord;
+      Y1      : in     Coord;
+      Z1      : in     Coord;
+      X2      :    out Coord;
+      Y2      :    out Coord;
+      R       : in     Coord;
+      M       : in     Coord;
+      M_Inv   : in     Types.Word32)
+   is
+      H : Coord;
+   begin
+      Invert (Z1, H, R, M, M_Inv);
+
+      Bignum.Mont_Mult
+        (X2, X2'First, X2'Last, X1, X1'First, H, H'First,
+         M, M'First, M_Inv);
+
+      Bignum.Mont_Mult
+        (Y2, Y2'First, Y2'Last, Y1, Y1'First, H, H'First,
+         M, M'First, M_Inv);
    end Make_Affine;
 
 end LSC.EC;
--- src/shared/generic/lsc-ec.ads
+++ src/shared/generic/lsc-ec.ads
@@ -14,6 +14,9 @@ is
    subtype Coord_Index is Natural range 0 .. Coord_Length - 1;
    subtype Coord is Bignum.Big_Int (Coord_Index);
 
+   One : constant Coord := Coord'(1, others => 0);
+   --# for One declare Rule;
+
    procedure Point_Double
      (X1       : in     Coord;
       Y1       : in     Coord;
@@ -124,6 +127,58 @@ is
    --#   Bignum.Num_Of_Big_Int (Z2, Z2'First, Z2'Length) <
    --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length);
 
+   procedure Two_Point_Mult
+     (X1       : in     Coord;
+      Y1       : in     Coord;
+      Z1       : in     Coord;
+      E1       : in     Bignum.Big_Int;
+      E1_First : in     Natural;
+      E1_Last  : in     Natural;
+      X2       : in     Coord;
+      Y2       : in     Coord;
+      Z2       : in     Coord;
+      E2       : in     Bignum.Big_Int;
+      E2_First : in     Natural;
+      X3       :    out Coord;
+      Y3       :    out Coord;
+      Z3       :    out Coord;
+      A        : in     Coord;
+      M        : in     Coord;
+      M_Inv    : in     Types.Word32);
+   --# derives
+   --#   X3, Y3, Z3 from
+   --#   X1, Y1, Z1, X2, Y2, Z2,
+   --#   E1, E1_First, E1_Last, E2, E2_First, A, M, M_Inv;
+   --# pre
+   --#   E1_First in E1'Range and
+   --#   E1_Last in E1'Range and
+   --#   E1_First <= E1_Last and
+   --#   E2_First in E2'Range and
+   --#   E2_First + (E1_Last - E1_First) in E2'Range and
+   --#   Bignum.Num_Of_Big_Int (X1, X1'First, X1'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   Bignum.Num_Of_Big_Int (Y1, Y1'First, Y1'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   Bignum.Num_Of_Big_Int (Z1, Z1'First, Z1'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   Bignum.Num_Of_Big_Int (X2, X2'First, X2'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   Bignum.Num_Of_Big_Int (Y2, Y2'First, Y2'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   Bignum.Num_Of_Big_Int (Z2, Z2'First, Z2'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   Bignum.Num_Of_Big_Int (A, A'First, A'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   1 < Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   1 + M_Inv * M (M'First) = 0;
+   --# post
+   --#   Bignum.Num_Of_Big_Int (X3, X3'First, X3'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   Bignum.Num_Of_Big_Int (Y3, Y3'First, Y3'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   Bignum.Num_Of_Big_Int (Z3, Z3'First, Z3'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length);
+
    procedure Make_Affine
      (X1      : in     Coord;
       Y1      : in     Coord;
@@ -148,5 +203,23 @@ is
    --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
    --#   1 < Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
    --#   1 + M_Inv * M (M'First) = 0;
+
+   procedure Invert
+     (A     : in     Coord;
+      B     :    out Coord;
+      R     : in     Coord;
+      M     : in     Coord;
+      M_Inv : in     Types.Word32);
+   --# derives
+   --#   B from A, R, M, M_Inv;
+   --# pre
+   --#   Bignum.Num_Of_Big_Int (R, R'First, R'Length) =
+   --#   Bignum.Base ** (2 * R'Length) mod
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   1 < Bignum.Num_Of_Big_Int (M, M'First, M'Length) and
+   --#   1 + M_Inv * M (M'First) = 0;
+   --# post
+   --#   Bignum.Num_Of_Big_Int (B, B'First, B'Length) <
+   --#   Bignum.Num_Of_Big_Int (M, M'First, M'Length);
 
 end LSC.EC;
