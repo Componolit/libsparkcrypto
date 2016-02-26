9bd69574b3da26453cfc39bafee449ddb48a4a3b
Stefan Berghofer <stefan.berghofer@secunet.com>
2016-02-26 15:49:48 +0100

    Adapted to Isabelle2016

--- src/theories/Mod_Simp.thy
+++ src/theories/Mod_Simp.thy
@@ -60,13 +60,13 @@ fun has_mod rng m s =
 
 fun pull_mod rng cmod cm ct =
   if Thm.term_of cm aconv Thm.term_of ct then
-    Drule.instantiate' [SOME (Thm.ctyp_of_cterm ct)] [SOME ct]
+    Thm.instantiate' [SOME (Thm.ctyp_of_cterm ct)] [SOME ct]
       @{thm mod_self_cong}
   else if has_mod rng (Thm.term_of cm) (Thm.term_of ct) then
     case Thm.term_of ct of
       Const (@{const_name mod}, _) $ _ $ _ =>
         let val (cx, cm) = Thm.dest_binop ct
-        in Drule.instantiate' [SOME (Thm.ctyp_of_cterm cx)] [SOME cx, SOME cm]
+        in Thm.instantiate' [SOME (Thm.ctyp_of_cterm cx)] [SOME cx, SOME cm]
           @{thm mod_mod_trivial}
         end
     | Const (@{const_name plus}, _) $ _ $ _ =>
@@ -86,7 +86,7 @@ fun pull_mod rng cmod cm ct =
         end
     | Const (@{const_name power}, _) $ _ $ _ =>
         let val (cl, cr) = Thm.dest_binop ct
-        in Drule.instantiate' [] [NONE, NONE, NONE, SOME cr]
+        in Thm.instantiate' [] [NONE, NONE, NONE, SOME cr]
           @{thm power_mod_cong} OF [pull_mod rng cmod cm cl]
         end
     | Const (@{const_name uminus}, _) $ _ =>
@@ -94,7 +94,7 @@ fun pull_mod rng cmod cm ct =
         in @{thm minus_mod_cong} OF [pull_mod rng cmod cm cu] end
     | _ => raise Fail "pull_mod"
   else
-    Drule.instantiate' [SOME (Thm.ctyp_of_cterm ct)]
+    Thm.instantiate' [SOME (Thm.ctyp_of_cterm ct)]
       [SOME (Drule.list_comb (cmod, [ct, cm]))] refl;
 
 fun pull_mod_proc _ ctxt ct = (case Thm.term_of ct of
