b94e55bc88ac23b5a1728340a7786c6219e30a96
Alexander Senier <senier@componolit.com>
2019-01-10 19:15:48 +0100

    Port SHA2 benchmarks
    Ref. #9

--- tests/lsc_benchmark.adb
+++ tests/lsc_benchmark.adb
@@ -36,12 +36,14 @@
 with LSC.Types;
 with LSC.RIPEMD160;
 with LSC.SHA1;
+with LSC.SHA256;
+with LSC.SHA512;
 with OpenSSL;
 with AUnit.Assertions; use AUnit.Assertions;
 with AUnit.Test_Results; use AUnit.Test_Results;
 
-
 use type LSC.Types.Word32_Array_Type;
+use type LSC.Types.Word64_Array_Type;
 
 package body LSC_Benchmark
 is
@@ -54,7 +56,7 @@ is
       Test_Time : constant Duration := T.Test_Stop - T.Test_Start;
       Percent   : constant Natural := Natural (Duration'(Test_Time / Ref_Time) * 100.0);
    begin
-      return Format (AUnit.Test_Cases.Test_Case (T).Routine_Name.all & "   [" & Percent'Img & "% ]");
+      return Format (AUnit.Test_Cases.Test_Case (T).Routine_Name.all & " ... [" & Percent'Img & "% ]");
    end Routine_Name;
 
    ---------------------------------------------------------------------------
@@ -132,6 +134,113 @@ is
 
    ---------------------------------------------------------------------------
 
+   procedure Benchmark_SHA256 (T : in out Test_Case'Class)
+   is
+      Block1, Block2  : LSC.SHA256.Block_Type;
+      SHA256_Context1 : OpenSSL.SHA256_Context_Type;
+      SHA256_Context2 : LSC.SHA256.Context_Type;
+      H1, H2          : LSC.SHA256.SHA256_Hash_Type;
+   begin
+      Block1  := LSC.SHA256.Block_Type'(others => 16#cafebabe#);
+      Block2  := LSC.SHA256.Block_Type'(others => 16#00636261#);
+
+      T.Reference_Start := Clock;
+      for I in Natural range 1 .. 500000
+      loop
+         OpenSSL.SHA256_Context_Init (SHA256_Context1);
+         OpenSSL.SHA256_Context_Update (SHA256_Context1, Block1);
+         OpenSSL.SHA256_Context_Finalize (SHA256_Context1, Block2, 56);
+      end loop;
+      H1 := OpenSSL.SHA256_Get_Hash (SHA256_Context1);
+      T.Reference_Stop := Clock;
+
+      T.Test_Start := Clock;
+      for I in Natural range 1 .. 500000
+      loop
+         SHA256_Context2 := LSC.SHA256.SHA256_Context_Init;
+         LSC.SHA256.Context_Update (SHA256_Context2, Block1);
+         LSC.SHA256.Context_Finalize (SHA256_Context2, Block2, 56);
+      end loop;
+      H2 := LSC.SHA256.SHA256_Get_Hash (SHA256_Context2);
+      T.Test_Stop := Clock;
+
+      Assert (H1 = H2, "Invalid hash");
+
+   end Benchmark_SHA256;
+
+   ---------------------------------------------------------------------------
+
+   procedure Benchmark_SHA384 (T : in out Test_Case'Class)
+   is
+      Block1, Block2  : LSC.SHA512.Block_Type;
+      SHA384_Context1 : OpenSSL.SHA384_Context_Type;
+      SHA384_Context2 : LSC.SHA512.Context_Type;
+      H1, H2          : LSC.SHA512.SHA384_Hash_Type;
+   begin
+      Block1  := LSC.SHA512.Block_Type'(others => 16#deadbeefcafebabe#);
+      Block2  := LSC.SHA512.Block_Type'(others => 16#0000000000636261#);
+
+      T.Reference_Start := Clock;
+      for I in Natural range 1 .. 500000
+      loop
+         OpenSSL.SHA384_Context_Init (SHA384_Context1);
+         OpenSSL.SHA384_Context_Update (SHA384_Context1, Block1);
+         OpenSSL.SHA384_Context_Finalize (SHA384_Context1, Block2, 56);
+      end loop;
+      H1 := OpenSSL.SHA384_Get_Hash (SHA384_Context1);
+      T.Reference_Stop := Clock;
+
+      T.Test_Start := Clock;
+      for I in Natural range 1 .. 500000
+      loop
+         SHA384_Context2 := LSC.SHA512.SHA384_Context_Init;
+         LSC.SHA512.Context_Update (SHA384_Context2, Block1);
+         LSC.SHA512.Context_Finalize (SHA384_Context2, Block2, 56);
+      end loop;
+      H2 := LSC.SHA512.SHA384_Get_Hash (SHA384_Context2);
+      T.Test_Stop := Clock;
+
+      Assert (H1 = H2, "Invalid hash");
+
+   end Benchmark_SHA384;
+
+   ---------------------------------------------------------------------------
+
+   procedure Benchmark_SHA512 (T : in out Test_Case'Class)
+   is
+      Block1, Block2  : LSC.SHA512.Block_Type;
+      SHA512_Context1 : OpenSSL.SHA512_Context_Type;
+      SHA512_Context2 : LSC.SHA512.Context_Type;
+      H1, H2          : LSC.SHA512.SHA512_Hash_Type;
+   begin
+      Block1  := LSC.SHA512.Block_Type'(others => 16#deadbeefcafebabe#);
+      Block2  := LSC.SHA512.Block_Type'(others => 16#0000000000636261#);
+
+      T.Reference_Start := Clock;
+      for I in Natural range 1 .. 500000
+      loop
+         OpenSSL.SHA512_Context_Init (SHA512_Context1);
+         OpenSSL.SHA512_Context_Update (SHA512_Context1, Block1);
+         OpenSSL.SHA512_Context_Finalize (SHA512_Context1, Block2, 56);
+      end loop;
+      H1 := OpenSSL.SHA512_Get_Hash (SHA512_Context1);
+      T.Reference_Stop := Clock;
+
+      T.Test_Start := Clock;
+      for I in Natural range 1 .. 500000
+      loop
+         SHA512_Context2 := LSC.SHA512.SHA512_Context_Init;
+         LSC.SHA512.Context_Update (SHA512_Context2, Block1);
+         LSC.SHA512.Context_Finalize (SHA512_Context2, Block2, 56);
+      end loop;
+      H2 := LSC.SHA512.SHA512_Get_Hash (SHA512_Context2);
+      T.Test_Stop := Clock;
+
+      Assert (H1 = H2, "Invalid hash");
+   end Benchmark_SHA512;
+
+   ---------------------------------------------------------------------------
+
    procedure Register_Tests (T: in out Test_Case) is
       package Registration is new
          AUnit.Test_Cases.Specific_Test_Case_Registration (Test_Case);
@@ -139,6 +248,9 @@ is
    begin
       Register_Wrapper (T, Benchmark_RIPEMD160'Access, "RIPEMD160");
       Register_Wrapper (T, Benchmark_SHA1'Access, "SHA1");
+      Register_Wrapper (T, Benchmark_SHA256'Access, "SHA256");
+      Register_Wrapper (T, Benchmark_SHA384'Access, "SHA384");
+      Register_Wrapper (T, Benchmark_SHA512'Access, "SHA512");
    end Register_Tests;
 
    ---------------------------------------------------------------------------
