ca5c778506491e7b49cbb6d0abf4f1900650f3ff
Alexander Senier <senier@componolit.com>
2019-02-28 19:44:54 +0100

    Fix RIPEMD160 hash and HMAC

--- src/shared/generic/lsc-ripemd160.ads
+++ src/shared/generic/lsc-ripemd160.ads
@@ -39,8 +39,11 @@ with LSC.RIPEMD160_Generic;
 
 package LSC.RIPEMD160
 is
+   subtype Hash_Index is Types.Natural_Index range 1 .. 20;
+   subtype Hash_Type is Types.Bytes (Hash_Index);
+
    function Hash is new RIPEMD160_Generic.Hash
       (Types.Natural_Index, Types.Byte, Types.Bytes,
-       Types.Natural_Index, Types.Byte, Types.Bytes);
+       Hash_Index, Types.Byte, Hash_Type);
 
 end LSC.RIPEMD160;
