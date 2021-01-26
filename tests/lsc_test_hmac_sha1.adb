8531a07b6e9f5eb33eae0fa32759b4cbd3509d95
Alexander Senier <senier@componolit.com>
2021-01-26 10:42:03 +0100

    Fix style violations

--- tests/lsc_test_hmac_sha1.adb
+++ tests/lsc_test_hmac_sha1.adb
@@ -1,4 +1,5 @@
--------------------------------------------------------------------------------  -- This file is part of libsparkcrypto.
+-------------------------------------------------------------------------------
+--  This file is part of libsparkcrypto.
 --
 --  Copyright (C) 2018 Componolit GmbH
 --  All rights reserved.
